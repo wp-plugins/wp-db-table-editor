68c599b4e3f28b0fb2d0c386bdb35b805691dc52
bobbysmith007 <bobbysmith007@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-04 20:05:03 +0000

    dequote id column in delete handler - it now seems autoquoted
    git-svn-id: https://plugins.svn.wordpress.org/wp-db-table-editor/trunk@1153279 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php-sql-parser/src/PHPSQL/Creator.php
+++ php-sql-parser/src/PHPSQL/Creator.php
@@ -413,8 +413,8 @@ class Creator {
             if ($len == strlen($sql)) {
                 throw new \PHPSQL\Exception\UnableToCreateSQL('function subtree', $k, $v, 'expr_type');
             }
-
-            $sql .= ($this->isReserved($v) ? " " : ",");
+            $sql .= ($this->isReserved($v) || ($parsed['base_expr'] == 'CAST')
+                     ? " " : ",");
         }
         return $parsed['base_expr'] . "(" . substr($sql, 0, -1) . ")";
     }
@@ -450,6 +450,7 @@ class Creator {
             $sql .= $this->processSubQuery($v);
             $sql .= $this->processSelectBracketExpression($v);
             $sql .= $this->processReserved($v);
+            $sql .= $this->processColRef($v);
 
             if ($len == strlen($sql)) {
                 throw new \PHPSQL\Exception\UnableToCreateSQL('expression subtree', $k, $v, 'expr_type');
@@ -471,6 +472,7 @@ class Creator {
             $sql .= $this->processColRef($v);
             $sql .= $this->processOperator($v);
             $sql .= $this->processConstant($v);
+            $sql .= $this->processFunction($v);
 
             if ($len == strlen($sql)) {
                 throw new \PHPSQL\Exception\UnableToCreateSQL('expression ref_clause', $k, $v, 'expr_type');
